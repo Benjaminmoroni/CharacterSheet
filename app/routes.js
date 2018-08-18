9c84fde99e067395687887311969b0dbee8900ad
Benjamin Jones <benjaminmoroni@gmail.com>
2018-08-17 19:24:24 -0600

    Created MYSQL database and modified routes to use database rather than JSON file to store character data

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,16 @@ var fs = require('fs');
 var path = require('path');
 var qs = require('querystring')
 var shortid = require("shortid")
-
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root1234",
+  database: "test"
+});
+con.connect(function(err) {
+  if (err) throw err;
+});
 var router = express.Router();
 module.exports = router;
 
@@ -12,14 +21,14 @@ router.get('/creation', function(req, res) {
   res.sendFile(path.join(__dirname, "../charactercreation.html"))
 });
 router.get('/sheets', function(req, res) {
-  let rawdata = fs.readFileSync("./characters.json");
-  let characters = JSON.parse(rawdata);
-  ejs.renderFile("charactersheet.html", {"characters":characters}, function(err, str){
-    res.write (str);
-    res.end(
-
-)
-  });
+    con.query("SELECT * FROM characters", function (err, result, fields) {
+      if (err) throw err;
+      let characters = result
+      ejs.renderFile("charactersheet.html", {"characters":characters}, function(err, str){
+        res.write (str);
+        res.end()
+      });
+    });
 })
 router.post('/update', function(req, res) {
   var body = "";
@@ -29,13 +38,11 @@ router.post('/update', function(req, res) {
   req.on("end", function(){
     var formData = qs.parse(body);
     var id = formData.characterId
-    fs.readFile('./characters.json', 'utf-8', function(err, data) {
-      if (err) throw err
-      var characters = JSON.parse(data)
-      characters[id] = formData
-      fs.writeFileSync('./characters.json', JSON.stringify(characters), 'utf-8', function(err) {
-        if (err) throw err
-      })
+    var sql = "UPDATE characters SET CharacterName = ?, Race = ?, Class = ?, Strength = ?, Dexterity = ?, Constitution = ?, Wisdom = ?, Intelligence = ?, Charisma = ? WHERE CharacterId = ?";
+
+    con.query(sql, [formData.CharacterName, formData.Race, formData.Class, formData.Strength, formData.Dexterity, formData.Constitution, formData.Wisdom, formData.Intelligence, formData.Charisma, formData.characterId], function (err, result, fields) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
     });
     res.writeHead(302, {'Location': '/character/sheets'});
     res.end();
@@ -50,13 +57,11 @@ router.post('/create', function(req, res) {
   req.on("end", function(){
     var formData = qs.parse(body);
     formData.id = newId
-    fs.readFile('./characters.json', 'utf-8', function(err, data) {
-      if (err) throw err
-      var characters = JSON.parse(data)
-      characters[newId] = formData
-      fs.writeFileSync('./characters.json', JSON.stringify(characters), 'utf-8', function(err) {
-        if (err) throw err
-      })
+    var sql = "INSERT INTO characters SET CharacterId = ?, CharacterName = ?, Race = ?, Class = ?, Strength = ?, Dexterity = ?, Constitution = ?, Wisdom = ?, Intelligence = ?, Charisma = ?";
+    //var values =
+    con.query(sql, [formData.id, formData.CharacterName, formData.Race, formData.Class, formData.Strength, formData.Dexterity, formData.Constitution, formData.Wisdom, formData.Intelligence, formData.Charisma], function (err, result, fields) {
+      if (err) throw err;
+      console.log("1 record inserted");
     });
     res.writeHead(302, {'Location': '/character/sheets'});
     res.end();
@@ -70,13 +75,11 @@ router.post('/delete', function(req, res) {
   req.on("end", function(){
     var formData = qs.parse(body);
     var id = formData.characterId
-    fs.readFile('./characters.json', 'utf-8', function(err, data) {
+    console.log(formData.characterId)
+    var sql = "DELETE FROM characters WHERE characterId = ?";
+    con.query(sql, [formData.characterId], function (err, result) {
       if (err) throw err;
-      var characters = JSON.parse(data);
-      delete characters[id];
-      fs.writeFileSync('./characters.json', JSON.stringify(characters), 'utf-8', function(err) {
-        if (err) throw err
-      });
+      console.log("Number of records deleted: " + result.affectedRows);
     });
     res.writeHead(302, {'Location': '/character/sheets'});
     res.end();
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var express = require('express');
+var ejs = require('ejs');
+var fs = require('fs');
+var path = require('path');
+var qs = require('querystring')
+var shortid = require("shortid")
+
 var app = express();
 var port = process.env.PORT || 8080;
 
@@ -6,3 +12,10 @@ var router = require('./app/routes.js');
 app.use('/character', router)
 
 app.listen(port)
+
+app.get('/', function (req, res) {
+  fs.readFile("login.html", function(err, data){
+    res.write (data)
+    res.end ()
+  })
+});
